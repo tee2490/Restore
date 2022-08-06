3ec975fa17fca2a3769088a5662fa4853ef5bb1a
Teeradete <tee2490@hotmail.com>
2022-08-06 15:48:13 +0700

    End of section 4 React Router

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -1,37 +1,48 @@
 import {
-    Container,
-    createTheme,
-    CssBaseline,
-    ThemeProvider,
-   } from "@mui/material";
-  import { useState } from "react";
-  import Catalog from "../../features/catalog/Catalog";
-  import Header from "./Header";
-  
-  export default function App() {
-    const [mode, setMode] = useState(false);
-    const modeDisplay = mode ? "dark" : "light";
-  
-    const handleMode = () => setMode(!mode);
-  
-    const theme = createTheme({
-      palette: {
-        mode: modeDisplay,
-        background: {
-          default: modeDisplay === "light" ? "#f0f5f7" : "#384348",
-        },
+  Container,
+  createTheme,
+  CssBaseline,
+  ThemeProvider,
+} from "@mui/material";
+import { useState } from "react";
+import { Route, Routes } from "react-router-dom";
+import AboutPage from "../../features/about/AboutPage";
+import Catalog from "../../features/catalog/Catalog";
+import ProductDetails from "../../features/catalog/ProductDetails";
+import ContactPage from "../../features/contact/ContactPage";
+import HomePage from "../../features/home/HomePage";
+import Header from "./Header";
+
+export default function App() {
+  const [mode, setMode] = useState(false);
+  const modeDisplay = mode ? "dark" : "light";
+
+  const handleMode = () => setMode(!mode);
+
+  const theme = createTheme({
+    palette: {
+      mode: modeDisplay,
+      background: {
+        default: modeDisplay === "light" ? "#f0f5f7" : "#384348",
       },
-    });
-  
-    return (
-      <>
-        <ThemeProvider theme={theme}>
-          <CssBaseline />
-          <Header handleMode={handleMode} />
-          <Container>
-            <Catalog />
-          </Container>
-        </ThemeProvider>
-      </>
-    );
-  }
+    },
+  });
+
+  return (
+    <>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Header handleMode={handleMode} />
+        <Container>
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/catalog" element={<Catalog />} />
+            <Route path="/catalog/:id" element={<ProductDetails />} />
+            <Route path="/about" element={<AboutPage />} />
+            <Route path="/contact" element={<ContactPage />} />
+          </Routes>
+        </Container>
+      </ThemeProvider>
+    </>
+  );
+}
--- client/src/app/layout/Header.tsx
+++ client/src/app/layout/Header.tsx
@@ -1,22 +1,93 @@
-import { AppBar, Box, Switch, Toolbar, Typography } from "@mui/material";
+import { ShoppingCart } from "@mui/icons-material";
+import {
+  AppBar,
+  Badge,
+  Box,
+  IconButton,
+  List,
+  ListItem,
+  Switch,
+  Toolbar,
+  Typography,
+} from "@mui/material";
+import { Link, NavLink } from "react-router-dom";
+
+const midLinks = [
+  { title: "catalog", path: "/catalog" },
+  { title: "about", path: "/about" },
+  { title: "contact", path: "/contact" },
+];
+
+const rightLinks = [
+  { title: "login", path: "/login" },
+  { title: "register", path: "/register" },
+];
+
+const navStyles = {
+  color: "inherit",
+  textDecoration: "none",
+  typography: "h6",
+  "&:hover": {
+    color: "grey.500",
+  },
+  "&.active": {
+    color: "text.secondary",
+  },
+};
 
 export default function Header(props: any) {
   return (
     <>
-      <AppBar position="static">
-        <Toolbar>
+      <AppBar position="static" sx={{ mb: 4 }}>
+        <Toolbar
+          sx={{
+            display: "flex",
+            justifyContent: "space-between",
+            alignItems: "center",
+          }}
+        >
           <Box display="flex" alignItems="center">
-            <Typography
-              variant="h6"
-              sx={{ color: "inherit", textDecoration: "none" }}
-            >
+            <Typography variant="h6" component={NavLink} to="/" sx={navStyles}>
               TEE-StORE
             </Typography>
             <Switch onChange={props.handleMode} />
           </Box>
+
+          <List sx={{ display: "flex" }}>
+            {midLinks.map(({ title, path }) => (
+              <ListItem component={NavLink} to={path} key={path} sx={navStyles}>
+                {title.toUpperCase()}
+              </ListItem>
+            ))}
+          </List>
+
+          <Box display="flex" alignItems="center">
+            <IconButton
+              component={Link}
+              to="/basket"
+              aria-label="cart"
+              sx={{ color: "inherit" }}
+              size="large"
+            >
+              <Badge badgeContent={4} color="warning">
+                <ShoppingCart />
+              </Badge>
+            </IconButton>
+            <List sx={{ display: "flex" }}>
+              {rightLinks.map(({ title, path }) => (
+                <ListItem
+                  component={NavLink}
+                  to={path}
+                  key={path}
+                  sx={navStyles}
+                >
+                  {title.toUpperCase()}
+                </ListItem>
+              ))}
+            </List>
+          </Box>
         </Toolbar>
       </AppBar>
-      <Toolbar />
     </>
   );
 }
